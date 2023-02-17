e3488065de1dd2cfcb68c6b4331d33718a3259f5
marw19 <yakoubi.marwen@esprit.tn>
2023-02-17 19:11:36 +0100

    crud fonctionnel

--- src/main/java/com/Debuggers/MobiliteInternational/Services/CandidacyService.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Services/CandidacyService.java
@@ -5,4 +5,9 @@ import com.Debuggers.MobiliteInternational.Repository.CandidacyRepository;
 import java.util.List;
 
 public interface CandidacyService {
+    List<Candidacy> getAllCandidancy();
+    public Candidacy getCandidancyById(long id);
+    public Candidacy addCandidature(Candidacy c);
+    public Candidacy updateCandidancy(Candidacy c);
+    public void deleteCandidancy(long id);
 }
---
+++ src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/ReportServiceImpl.java
@@ -0,0 +1,4 @@
+package com.Debuggers.MobiliteInternational.Services.Impl;
+
+public class ReportServiceImpl {
+}
