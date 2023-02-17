a4e28f17c600ed0878c4be53050dc5eba3e6b8fd
unknown <mohamedamine.aouini@esprit.tn>
2023-02-17 23:51:23 +1300

    CRUD Valid

--- src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/DormServiceImpl.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/DormServiceImpl.java
@@ -1,8 +1,40 @@
 package com.Debuggers.MobiliteInternational.Services.Impl;
 
+import com.Debuggers.MobiliteInternational.Entity.Dormitories;
+import com.Debuggers.MobiliteInternational.Repository.DormRepository;
 import com.Debuggers.MobiliteInternational.Services.DormService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class DormServiceImpl implements DormService {
+    DormRepository dormitoriesRepository;
+
+    @Override
+    public List<Dormitories> getAllDorm() {
+        return dormitoriesRepository.findAll();
+    }
+
+    @Override
+    public Dormitories getDormById(long id) {
+        return dormitoriesRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Dormitories addDorm(Dormitories d) {
+        return dormitoriesRepository.save(d);
+    }
+
+    @Override
+    public Dormitories UpdateDorm(Dormitories d) {
+        return dormitoriesRepository.save(d);
+    }
+
+    @Override
+    public void deleteDorm(long id) {
+        dormitoriesRepository.deleteById(id);
+    }
 }
