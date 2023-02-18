7f61ea0c0170bc3508e52b406ee45bdc5d8db12f
FirasDridi <mohamedfiras.dridi@esprit.com>
2023-02-18 01:12:51 +0100

    crud fonctionnel

--- src/main/java/com/Debuggers/MobiliteInternational/Controllers/CommentController.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Controllers/CommentController.java
@@ -1,7 +1,42 @@
 package com.Debuggers.MobiliteInternational.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Debuggers.MobiliteInternational.Entity.Comment;
+import com.Debuggers.MobiliteInternational.Services.CommentService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class CommentController {
+
+    @AllArgsConstructor
+    public class CommentController {
+        CommentService commentService;
+
+
+        @GetMapping("/allcomment")
+        public List<Comment> getAllComments() {
+            return commentService.getAllComments();
+        }
+
+        @GetMapping("/getc{id}")
+        public Comment getCommentById(@PathVariable Long id) {
+            return commentService.getCommentById(id);
+        }
+
+        @PostMapping("/addc")
+        public void addComment(@RequestBody Comment comment) {
+            commentService.addComment(comment);
+        }
+
+        @PutMapping("/upc")
+        public void updateComment(@RequestBody Comment comment) {
+            commentService.updateComment(comment);
+        }
+
+        @DeleteMapping("/delc{id}")
+        @ResponseBody
+        public void deleteComment(@PathVariable("id") Long id){
+            commentService.deleteComment(id);
+        }
 }
--- src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/PublicationServiceImp.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/PublicationServiceImp.java
@@ -1,8 +1,40 @@
 package com.Debuggers.MobiliteInternational.Services.Impl;
 
+import com.Debuggers.MobiliteInternational.Entity.Post;
+import com.Debuggers.MobiliteInternational.Repository.PublicationRepository;
 import com.Debuggers.MobiliteInternational.Services.PublicationService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class PublicationServiceImp implements PublicationService {
+    private final PublicationRepository publicationRepository;
+
+    @Override
+    public List<Post> getAllPublication() {
+        return publicationRepository.findAll();
+    }
+
+    @Override
+    public Post getPublicationById(Long id) {
+        return publicationRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void addPublication(Post post) {
+        publicationRepository.save(post);
+    }
+
+    @Override
+    public void updatePublication(Post post) {
+        publicationRepository.save(post);
+    }
+
+    @Override
+    public void deletePublication(Long id) {
+        publicationRepository.deleteById(id);
+    }
 }
