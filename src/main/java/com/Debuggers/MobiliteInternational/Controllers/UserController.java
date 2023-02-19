57decadeb83a3e12f17829e08ef439f2722198f9
Ellouminati <abdellaziz.elloumi@esprit.tn>
2023-02-19 17:34:26 +0100

    crud user

--- src/main/java/com/Debuggers/MobiliteInternational/Controllers/UserController.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Controllers/UserController.java
@@ -1,8 +1,49 @@
 package com.Debuggers.MobiliteInternational.Controllers;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.Debuggers.MobiliteInternational.Entity.User;
+import com.Debuggers.MobiliteInternational.Repository.UserRepository;
+import com.Debuggers.MobiliteInternational.Services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
+
 public class UserController {
+    UserService userService;
+    private final UserRepository userRepository;
+
+
+    @GetMapping("/getAllUsers")
+    @ResponseBody
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
+
+
+    @PostMapping("/users/{idRole}")
+    @ResponseBody
+    public User saveUser(@RequestBody User user, @PathVariable("idRole") Long idRole){
+        return userService.save(user,idRole);
+
+    }
+
+
+    @PutMapping("/users/{userId}")
+    @ResponseBody
+    public User updateUser(@PathVariable Long userId, @RequestBody User user){
+        return userService.update(user);
+    }
+
+
+    @DeleteMapping("/users/{id}")
+    @ResponseBody
+    public void deleteUser(@PathVariable Long id){
+        userService.delete(id);
+    }
+
+
 }
--- src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/UserServiceImpl.java
+++ src/main/java/com/Debuggers/MobiliteInternational/Services/Impl/UserServiceImpl.java
@@ -1,9 +1,84 @@
 package com.Debuggers.MobiliteInternational.Services.Impl;
 
+import com.Debuggers.MobiliteInternational.Entity.Role;
+import com.Debuggers.MobiliteInternational.Entity.User;
+import com.Debuggers.MobiliteInternational.Repository.RoleRepository;
+import com.Debuggers.MobiliteInternational.Repository.UserRepository;
 import com.Debuggers.MobiliteInternational.Services.UniversityService;
 import com.Debuggers.MobiliteInternational.Services.UserService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+
+    RoleRepository roleRepository;
+    @Override
+    public List<User> getAllUsers() {
+        return
+                userRepository.findAll();
+    }
+
+    @Override
+    public User save(User utilisateur,Long idRole) {
+
+
+        Role role = roleRepository.findById(idRole).orElse(null);
+        utilisateur.setRole(role);
+        return userRepository.save(utilisateur);
+
+
+    }
+
+    @Override
+    public User update(User utilisateur) {
+
+        return userRepository.save(utilisateur);
+    }
+
+    @Override
+    public void delete(Long code) {
+        User u = getUserById(code);
+
+        if(u==null)
+        {
+            System.out.println("not found");
+        }
+        userRepository.delete(u);
+
+
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        User utilisateur = userRepository.findById(id).orElse(null);
+        return utilisateur;
+    }
+
+    @Override
+    public void assignRolesToUser(Long idUser, Long idRole) {
+        User user = userRepository.findById(idUser).get();
+        Role role = roleRepository.findById(idRole).get();
+
+        user.setRole(role);
+        userRepository.save(user);
+    }
+
+    @Override
+    public int findByUserRoleAdmin() {
+        return 0;
+    }
+
+    @Override
+    public int findByUserRoleEmployee() {
+        return 0;
+    }
+
+    @Override
+    public int findByUserRoleManager() {
+        return 0;
+    }
 }
